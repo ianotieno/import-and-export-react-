42a387f02d80305ce9970417c1e3d4b6610e96fa
ianotieno <ianotieno23@gmail.com>
2023-07-19 14:21:02 +0300

    updated

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,20 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife, elevation } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks();  // logs "42 parks!"
+  wildlife();  // logs "Elk, Bighorn Sheep, Moose"
+  elevation();  // logs "9583 ft"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      <p>Trees found: {trees}</p>
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
